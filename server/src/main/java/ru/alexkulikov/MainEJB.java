77287ec78b4497c13772011061c96700e28639da
WatsOne <thewato@gmail.com>
2016-07-20 20:07:54 +0500

    stable example

--- server/src/main/java/ru/alexkulikov/MainEJB.java
+++ server/src/main/java/ru/alexkulikov/MainEJB.java
@@ -5,13 +5,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/sss")
+@Path("/")
 @Stateless
 public class MainEJB {
 
-    @Path("/xxx")
+    @Path("/echo")
     @GET
     public Response doEcho() {
-        return Response.ok("HELLO").build();
+        return Response.ok("HELLO WORLD!").build();
     }
 }
